ad074b65fea0f6cce3cbe5fecc79f166f1c9b563
kartik-nv <68604809+kartik-nv@users.noreply.github.com>
2020-12-20 12:42:20 +0530

    Delete SignUp.js

--- js/SignUp.js
+++
@@ -1,9 +0,0 @@
-function checkPassword() {
-    var pass = document.forms["login"]["Pass"].value;
-    var repass = document.forms["login"]["RePass"].value;
-    if(pass != repass)
-    {
-            alert("Password don't Match!!");
-            return false;
-    }
-}
